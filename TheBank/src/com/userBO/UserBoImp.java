1ddfcd4739f032d6d930b1530596dfe80ab3a2f0
Tom Bonner <zerosavant@yahoo.com>
2019-12-28 21:38:21 -0600

    added abstracts and stubs for Users

--- TheBank/src/com/userBO/UserBoImp.java
+++ TheBank/src/com/userBO/UserBoImp.java
@@ -1,5 +1,87 @@
 package com.userBO;
 
+import java.util.List;
+
+import com.bank.to.User;
+
 public class UserBoImp implements UserBO {
+
+	@Override
+	public User getUserById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByFullName(String firstName, String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByArchetype(int archetype) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserBySSN(String ssn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByHomePhone(String phoneNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByMobilePhone(String phoneNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByCityState(String city, String state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByState(String city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByCountry(String country) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByZip(String zip) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getUsersByDateCreated() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- TheBank/src/com/userDAO/UserDAO.java
+++ TheBank/src/com/userDAO/UserDAO.java
@@ -1,5 +1,21 @@
 package com.userDAO;
 
-public interface UserDAO {
+import java.util.List;
 
+import com.bank.to.User;
+
+public interface UserDAO {
+	User getUserById(int id);
+	List<User> getUsersByFullName(String firstName, String lastName);
+	List<User> getUsersByArchetype(int archetype);
+	User getUserBySSN(String ssn);
+	List<User> getUsersByHomePhone(String phoneNumber);
+	List<User> getUsersByMobilePhone(String phoneNumber);
+	List<User> getUsersByEmail(String email);
+	List<User> getUsersByCityState(String city, String state);
+	List<User> getUsersByState(String city);
+	List<User> getUsersByCountry(String country);
+	List<User> getUsersByZip(String zip);
+	User getUserByUsername(String username);
+	List<User> getUsersByDateCreated();
 }
